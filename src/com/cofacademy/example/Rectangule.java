e9ea1d02d1d372175f36abca87b33f965d13d482
Ivan Rubio <smashedpurse@gmail.com>
2022-12-29 16:58:32 -0600

    Develope a new class extended from the Abstract class and write the same sintaxis using the respective formula

--- src/com/cofacademy/example/Rectangule.java
+++ src/com/cofacademy/example/Rectangule.java
@@ -1,4 +1,22 @@
 package com.cofacademy.example;
 
-public class Rectangule {
+public class Rectangule extends Figure{
+
+    private double base;
+    private double large;
+
+    public void setBase(double base) {
+        this.base = base;
+    }
+
+    public void setLarge(double large) {
+        this.large = large;
+    }
+
+    @Override
+    public double calculateArea() {
+        double result = base * large / 2 ;
+        return result;
+    }
 }
+
