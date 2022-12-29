28b5ed3fea094e434fe0d18e8fe63160eeb114ac
Ivan Rubio <smashedpurse@gmail.com>
2022-12-29 17:04:02 -0600

    Use of the Rectangule class than is a subClass from the abstract class Figure to get the area of a rectangule

--- src/com/cofacademy/example/Main.java
+++ src/com/cofacademy/example/Main.java
@@ -2,12 +2,24 @@ package com.cofacademy.example;
 
 public class Main {
     public static void main(String[] args) {
+
+        //APPLICATION OF THE SUBCLASS USING A METHOD PROVIDED BY A ABSTRACT CLASS
         Square s = new Square ();
-        //s.display();
-
         s.setSide(23);
-
         System.out.println("The area of the square is: " + s.calculateArea());
+
+        //APPLICATION OF THE SUBCLASS USING A METHOD PROVIDED BY A ABSTRACT CLASS
+        Rectangule r = new Rectangule();
+        r.setBase(10);
+        r.setLarge(2);
+        r.calculateArea();
+        System.out.println("The area of the rectangule is: " + r.calculateArea());
+
+
+
+
+
+
 
 
 
--- src/com/cofacademy/example/Rectangule.java
+++ src/com/cofacademy/example/Rectangule.java
@@ -1,21 +1,23 @@
 package com.cofacademy.example;
-
 public class Rectangule extends Figure{
 
+    //DECLARATION OF VARIABLES
     private double base;
     private double large;
 
+    //DECLARATION OF THE SETTER FOR THE BASE
     public void setBase(double base) {
         this.base = base;
     }
-
+    //DECLARATION OF THE SETTER FOR THE LARGE
     public void setLarge(double large) {
         this.large = large;
     }
 
+    //OVERRIDE OF THE METHOD calculareArea PROVIDED BY THE ABSTRACT CLASS FIGURE
     @Override
     public double calculateArea() {
-        double result = base * large / 2 ;
+        double result = base * large;
         return result;
     }
 }
